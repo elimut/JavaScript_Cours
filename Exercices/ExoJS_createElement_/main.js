312b2a5f95361e505bb6c6c4458524b895d04f9e
elimut <121231529+elimut@users.noreply.github.com>
2023-03-22 17:04:15 +0100

    exo

--- Exercices/ExoJS_createElement_/main.js
+++ Exercices/ExoJS_createElement_/main.js
@@ -27,6 +27,7 @@
 let inputValue = "";
 let affichage = document.createElement("form");
 let affichage1 = document.createElement("input");
+affichage1.setAttribute("type","text");
 affichage.append(affichage1);
 document.body.append(affichage);
 let affichage2 = document.createElement("p");
